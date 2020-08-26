19d58e7bfebbcae7db01f6d73da52e6ebe4e559a
Lucas Jesus <cc19372@g.unicamp.br>
2020-08-26 08:01:20 -0300

    create component social media and border data user

--- my-web-site/src/app/app.module.ts
+++ my-web-site/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BorderDataComponent } from './components/border-data/border-data.component';
+import { SocialMediaComponent } from './components/social-media/social-media.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BorderDataComponent,
+    SocialMediaComponent
   ],
   imports: [
     BrowserModule
---
+++ my-web-site/src/app/components/border-data/border-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-border-data',
+  templateUrl: './border-data.component.html',
+  styleUrls: ['./border-data.component.css']
+})
+export class BorderDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
