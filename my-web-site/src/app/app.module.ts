feb3c602fd3432c82705979539b3333127a6cf2a
luccas traumer <cc19372@g.unicamp.br>
2020-08-28 19:22:17 -0300

    init build layout feed

--- my-web-site/src/app/app.module.ts
+++ my-web-site/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BorderDataComponent } from './components/border-data/border-data.component';
 import { SocialMediaComponent } from './components/social-media/social-media.component';
+import { FeedComponent } from './components/feed/feed.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BorderDataComponent,
+    FeedComponent,
     SocialMediaComponent
   ],
   imports: [
