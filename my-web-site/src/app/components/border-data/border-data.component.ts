a07513b779641654735d9889a36292daef4ce390
Lucas Jesus <cc19372@g.unicamp.br>
2020-08-30 15:38:26 -0300

    fix someones death code and fix border

--- my-web-site/src/app/components/border-data/border-data.component.ts
+++ my-web-site/src/app/components/border-data/border-data.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-border-data',
   templateUrl: './border-data.component.html',
-  styleUrls: ['./border-data.component.css']
+  styleUrls: ['../../styles/styles.scss', './border-data.component.css']
 })
 export class BorderDataComponent implements OnInit {
 
