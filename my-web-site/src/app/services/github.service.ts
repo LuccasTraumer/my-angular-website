5f20a9933654328c462ba705453c999ab1f70928
Lucas Jesus <cc19372@g.unicamp.br>
2020-09-02 08:00:53 -0300

    created a var in env file and cretaed service to make request and get data of github

---
+++ my-web-site/src/app/services/github.service.ts
@@ -0,0 +1,15 @@
+import { environment } from './../../environments/environment';
+import { Injectable, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubService {
+
+  constructor(private http: HttpClient) { }
+
+  OnInit() {
+    environment.url_github;
+  }
+}
